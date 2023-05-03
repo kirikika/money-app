75a3198a5caf7939d79ab79fdb5a421bcd05c98b
SponsorM2 <gmjuffin@gmail.com>
2023-05-03 23:14:47 +0300

    layout main page

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
-
+export class Tab1Page implements OnInit {
+  public name: string = 'Vladislav';
+  public isShowTransaction: boolean = false;
   constructor() {}
 
+  ngOnInit() {
+    setTimeout(() => {
+      this.name = 'Vadim';
+    }, 5000)
+  }
+
+  public showTransaction() {
+    this.isShowTransaction = !this.isShowTransaction;
+  }
 }
